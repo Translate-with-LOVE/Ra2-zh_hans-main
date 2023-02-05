e85d6205dea79ae710513559907d757fc9f1b76f
Handle <mail@bysb.net>
2023-02-05 23:52:27 +0800

    Bace CI

---
+++ .github/scripts/download_latest_csf.cmd
@@ -0,0 +1,2 @@
+curl https://weblate.usvcs.cn/download/redalert2_yr_chs/ra2md-csf/zh_Hans/ -o ../temp/ra2md.csf.ini
+curl https://weblate.usvcs.cn/download/redalert2_yr_chs/ra2-csf/zh_Hans/ -o ../temp/ra2.csf.ini
